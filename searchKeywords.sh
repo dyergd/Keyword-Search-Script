ea13c37d5796432b9bbe2beff264fe2fd3be774d
Garrett Dyer <70722571+dyergd@users.noreply.github.com>
2022-07-12 16:40:43 -0400

    Made changes to script
    changed the format of the script and added comments

--- searchKeywords.sh
+++ searchKeywords.sh
@@ -1,32 +1,36 @@
-i=1;
-k=1;
-if [ "$1" = "-all" ]
+i=1; #counter that determines the number of tweets that contain the search term
+k=1; #another counter that determines the number of tweets that contain the search term
+
+#if the first option in the statement is -all
+#the script will take the 2 search terms and create a folder that contains tweets using either term
+if [ "$1" = "-all" ] 
 then
-mkdir $2_$3;
-grep -wi -e $2 -e $3  ~/final/tweet.txt | while read -r tweet
-do
-echo $tweet > tweet_$i.txt;
-mv "tweet_$i.txt" $2_$3/"tweet_$i.txt";
-i=$(( $i + 1 ))
-done
+    mkdir $2_$3; #creates a directory named after ther two search terms
+    grep -wi -e $2 -e $3  ~/final/tweet.txt | while read -r tweet #grabs tweets contain either search term and puts them into a varible
+    do
+        echo $tweet > tweet_$i.txt; #each tweet is then copied into a text file
+        mv "tweet_$i.txt" $2_$3/"tweet_$i.txt"; #the text file containing the tweet is then moved into the designated folder
+        i=$(( $i + 1 ))
+    done
 else
-mkdir $1;
-grep -wi $1 ~/final/tweet.txt |while read -r tweet
+    mkdir $1;
+    grep -wi $1 ~/final/tweet.txt | while read -r tweet
 do
-echo $tweet > tweet_$i.txt;
-mv tweet_$i.txt $1/tweet_$i.txt
-i=$(( $i + 1 ))
+    echo $tweet > tweet_$i.txt;
+    mv tweet_$i.txt $1/tweet_$i.txt;
+    i=$(( $i + 1 ))
 done
+
 if [ -z $2 ]
 then
-:
+    :
 else
-mkdir $2;
-grep -wi $2 ~/final/tweet.txt |while read -r tweets
+    mkdir $2;
+    grep -wi $2 ~/final/tweet.txt | while read -r tweets
 do
-echo $tweets > tweet_$k.txt;
-mv tweet_$k.txt $2/tweet_$k.txt;
-k=$(( $k + 1 ))
+    echo $tweets > tweet_$k.txt;
+    mv tweet_$k.txt $2/tweet_$k.txt;
+    k=$(( $k + 1 ))
 done
 fi
 fi
